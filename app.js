51358d19a587d9c7169c4ac92136fc8009ae6876
Sahilkumar0011 <114636616+Sahilkumar0011@users.noreply.github.com>
2025-01-13 14:17:42 +0530

    new commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv= require("dotenv")
 const colors= require("colors")
 const path= require('path')
 const connectdb = require("./config/connectdb")
+const cookieParser = require("cookie-parser")
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'))
 app.use(cors())
+app.use(cookieParser)
 
 
 // user routes
